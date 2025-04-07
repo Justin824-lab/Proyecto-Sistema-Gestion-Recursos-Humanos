964e5d8bb307010faac2b8dd7ad2e30f229c8b4b
Justin <marillocandelarios@gmail.com>
2025-04-07 00:15:14 -0600

    arreglos en tablas

--- frontend/src/app/components/empleados/empleados.component.ts
+++ frontend/src/app/components/empleados/empleados.component.ts
@@ -34,7 +34,7 @@ export class EmpleadosComponent implements OnInit  {
       let PDF = new jsPDF('p', 'mm', 'a4');
       let position = 0;
       PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight);
-      PDF.save('empresas.pdf');
+      PDF.save('empleados.pdf');
     });
   }
 
